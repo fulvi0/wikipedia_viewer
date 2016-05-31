1fa51db20134cd0dfdb06e9e7dbf66cc2b107d57
Carlos Antonio <fulvio.ac@gmail.com>
2016-05-30 21:07:32 -0400

    change url api

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-  var url = "https://gist.githubusercontent.com/fulvi0/0ee486d83d7c533c844366a19f77dd3d/raw/e7f0e4d0bd784c5ef50c926faa568077b2a438b2/fr_quotes.json";
+  var searchBox = '';
+  var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&titles=Main+Page&generator=search&gsrsearch=" + searchBox + "&callback=?";
 
   var extractor = function(result) {
     $.each(result, function(key, value) {
